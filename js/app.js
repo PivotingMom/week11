b42fb187b366d42208ec653fb545d719d87bf9e8
Pivotingmum <joladeosuntubo@gmail.com>
2022-04-02 13:58:05 -0600

    week 11a

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+function changeBackground() {
+  let text = document.getElementById("plex").style.backgroundColor = "red";
+}
+
+function handleClick(element){
+  element.innerHTML = "clicked"
+}
+
+
+const y = document.getElementById("hoverPara");
+  y.addEventListener("mouseover", function hover() {style.backgroundColor = "orange"; 
+  });
