3806e5987bb07e05ed9a08ca14e0755809098fb6
Pivotingmum <joladeosuntubo@gmail.com>
2022-04-02 17:00:28 -0600

    week 11a

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 
 function changeBackground() {
-  let text = document.getElementById("plex").style.backgroundColor = "red";
+  console.log("late");
+  document.getElementById("hoverPara").style.backgroundColor = "red";
 }
+
+
+const y = document.getElementById("hoverPara");
+  y.addEventListener("mouseover", changeBackground);
+
+
+
+  function changeBackground() {
+  console.log("welcome");
+  document.getElementById("plex").style.backgroundColor = "green";
+}
+
+const x = document.getElementById("green");
+  y.addEventListener("mouseover", changeBackground);
 
 function handleClick(element){
   element.innerHTML = "clicked"
 }
 
+//add eventListener  mouseover  target,addEventListener("eventname", "function", true/false)//
 
-const y = document.getElementById("hoverPara");
-  y.addEventListener("mouseover", function hover() {style.backgroundColor = "orange"; 
-  });
+const keyboard = document.querySelector('.keyboard');
+
+keyboard.addEventListener('keydown', e =>{
+  console.log(e)
+  if (e.keyCode === 32){
+    alert ('i figured it out' + e.key ), e.preventDefault()
+  }
+})
+  
